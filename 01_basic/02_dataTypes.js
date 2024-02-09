41fa1768a8f0ef2f45b78fa644bb6c1c4e6ecd60
RAshid602 <19051001@kiit.ac.in>
2024-02-09 18:38:34 +0545

    basic fo type conversion

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,7 @@
+// in js there same as other 
+// string, boolean, int same like others
+// but we dont have to write it 
+let name1="rashid" // string
+let name2= 22; // number
+
+console.log(typeof name1); // it will show the type of name1
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score1 = "33";
+let score2 = "33abd"; // yah wala convert ho jaiga but value will not be accurate
+
+let scoreInNumber = Number(score2);
+// console.log(scoreInNumber); // answer will be NAN 
+
+
+let isLoggedIn = "0";
+let canBeBoolean = Boolean(isLoggedIn);
+console.log(canBeBoolean);
